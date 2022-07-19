3c2adf9a63e0cfd3307d88d7c344993e0593951a
shakil18046 <73681343+shakil18046@users.noreply.github.com>
2022-07-19 12:15:07 +0600

    setup daisyui and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#ED3AB2",
+        
+"secondary": "#952DDE",
+        
+"text-main-color": "#333333",
+        
+"text-secendory-color": "#555555",
+        
+"base-100": "#FFFFFF",
+        
+"bottom-footer": "#411361",
+        
+"main-footer": "#4D0C7A",
+        
+"contact": "#952DDE",
+        
+"border": "#ed3ab26b",
+
+"error" : "#fc0303"
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
