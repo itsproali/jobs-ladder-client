29c331e85e8733b27e972ba7099eb232631bf7df
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-07-19 22:53:01 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import RoutesIndex from './components/Routes-Setup/Router';
+import "./App.css";
+import RoutesIndex from "./components/Routes-Setup/Router";
+import Footer from "./pages/Home/Footer/Footer";
 
 function App() {
   return (
     <div>
       <RoutesIndex></RoutesIndex>
+      <Footer></Footer>
     </div>
   );
 }
