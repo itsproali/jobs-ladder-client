5d5600d260edd938629392d672c6d0ee3cb7dc15
checkiamsiam <issiam02415@gmail.com>
2022-07-19 19:31:22 +0600

    1 section added

--- src/pages/Home/TrustedBy/TrustedBy.js
+++ src/pages/Home/TrustedBy/TrustedBy.js
@@ -17,8 +17,8 @@ const TrustedBy = () => {
   const companysLogo = [jdc, tesla, meta, findelity, monster, nueco, ocado, penstate, sap, converse, microsoft, google]
 
   return (
-    <div className='container mx-auto px-5 '>
-      <div className='flex flex-col sm:gap-3 gap-1 justify-center items-center mb-5'>
+    <div className='container mx-auto px-5'>
+      <div className='flex flex-col sm:gap-3 gap-1 justify-center items-center sm:mb-10 mb-5'>
         <h5 className='font-bold uppercase text-primary sm:text-lg text-sm'>we are trusted</h5>
         <h1 className=' md:text-3xl sm:text-2xl text-lg font-bold uppercase flex items-center gap-[2px]'>Trusted By hundred<span className='font-extrabold'>+</span> <span className='sm:block hidden'> companies</span></h1>
         <div className='flex sm:gap-2 gap-1 mt-1'>
@@ -26,9 +26,9 @@ const TrustedBy = () => {
           <div className='sm:h-1 h-[2px]  sm:w-7 w-4 bg-primary rounded-lg'></div>
         </div>
       </div>
-      <div className='flex sm:gap-0 gap-5 flex-wrap justify-center items-center  '>
+      <div className='flex sm:gap-0 gap-5 flex-wrap justify-center items-center relative'>
         {companysLogo.map((logo, index) =>
-          <div key={index} className='flex overflow-hidden companys-logo mr-3 gap-3 justify-center items-center sm:p-5  sm:w-48 sm:h-24 w-20  rounded-lg transition duration-500 relative'>
+          <div key={index} className='flex mb-3 overflow-hidden companys-logo mr-3 gap-3 justify-center items-center sm:p-5  sm:w-48 sm:h-24 w-20  rounded-lg transition duration-500 relative  z-50 '>
             <img  src={logo} alt="" className='w-full relative z-50' />
           </div>
         )}
