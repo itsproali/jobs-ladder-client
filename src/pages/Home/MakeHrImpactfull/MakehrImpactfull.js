177157a09c4dcd7698fbb5be9c62f03ea8c4bcdf
checkiamsiam <issiam02415@gmail.com>
2022-07-20 10:41:23 +0600

    section added

--- src/pages/Home/MakeHrImpactfull/MakehrImpactfull.js
+++ src/pages/Home/MakeHrImpactfull/MakehrImpactfull.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BsArrowRight } from 'react-icons/bs';
+import circle1 from "../../../asset/testimonial/circle-1.png";
+import circle2 from "../../../asset/testimonial/circle-2.png";
+import search from "../../../asset/servicesForHr/search.png";
+import calander from "../../../asset/servicesForHr/calander-mark.png";
+import handshake from "../../../asset/servicesForHr/handshake.png";
+import typing from "../../../asset/servicesForHr/typing.png";
+import './makeHrImapactfull.css'
 
 const MakehrImpactfull = () => {
   return (
-    <div className='container mx-auto mt-10'>
-        <div className='flex flex-col sm:gap-3 gap-1 justify-center items-center mb-5'>
+    <div className='container mx-auto mt-10 relative px-5'>
+      <div className='flex flex-col sm:gap-3 gap-1 justify-center items-center mb-5 pb-5'>
         <h5 className='font-bold uppercase text-primary sm:text-lg text-sm'>It's Impactful</h5>
         <h1 className=' md:text-3xl sm:text-2xl text-lg font-bold uppercase'>Smart Solution and affordable</h1>
         <div className='flex sm:gap-2 gap-1 mt-1'>
@@ -11,8 +19,57 @@ const MakehrImpactfull = () => {
           <div className='sm:h-1 h-[2px]  sm:w-7 w-4 bg-primary rounded-lg'></div>
         </div>
       </div>
-      <div>
+      <div className='grid xl:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-5 justify-between'>
 
+
+        <div class="sm:h-[400px]  shadow-xl impactfull-card rounded-lg flex justify-center items-center cursor-pointer">
+          <div className=''>
+            <div className=' flex justify-center'><img src={search} alt="" className='w-40' /></div>
+            <div className='h-[200px]'>
+              <h1 className='text-xl font-extrabold text-center '>Automate repetitive routines</h1>
+              <p className='md:text-xl font-semibold text-center my-3 '> Reduce recruiter workload by as much as 75%.</p>
+              <p className='flex gap-1 hover:gap-2 text-secondary font-semibold justify-center items-center'>Application Tracing System <BsArrowRight /></p>
+            </div>
+          </div>
+        </div>
+        <div class="sm:h-[400px]  shadow-xl impactfull-card rounded-lg flex justify-center items-center cursor-pointer">
+          <div className=''>
+            <div className=' flex justify-center'><img src={handshake} alt="" className='w-40' /></div>
+            <div className='h-[200px]'>
+              <h1 className='text-xl font-extrabold text-center '>Onboard faster</h1>
+              <p className='md:text-xl font-semibold text-center my-3 '> Cut down on new hire onboarding queries.</p>
+              <p  className='flex gap-1 hover:gap-2 text-secondary font-semibold justify-center items-center'>Virtual Onboarding <BsArrowRight /></p>
+            </div>
+          </div>
+        </div>
+        <div class="sm:h-[400px]  shadow-xl impactfull-card rounded-lg flex justify-center items-center cursor-pointer">
+          <div className=''>
+            <div className=' flex justify-center'><img src={typing} alt="" className='w-40' /></div>
+            <div className='h-[200px]'>
+              <h1 className='text-xl font-extrabold text-center '>Organize data better</h1>
+              <p className='md:text-xl font-semibold text-center my-3 '> Securely store all employee data and files.</p>
+              <p className='flex gap-1 hover:gap-2 text-secondary font-semibold justify-center items-center'>Employee Database <BsArrowRight /></p>
+            </div>
+          </div>
+        </div>
+        <div class="sm:h-[400px]  shadow-xl impactfull-card rounded-lg flex justify-center items-center cursor-pointer">
+          <div className=''>
+            <div className=' flex justify-center'><img src={calander} alt="" className='w-40' /></div>
+            <div className='h-[200px]'>
+              <h1 className='text-xl font-extrabold text-center '>Manage time off easier</h1>
+              <p className='md:text-xl font-semibold text-center my-3 '> Track and report time-off and absence.</p>
+              <p className='flex gap-1 hover:gap-2 text-secondary font-semibold justify-center items-center'>Time off management <BsArrowRight /></p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+      <div className="hidden md:block absolute left-8 w-32 -bottom-12 -z-10">
+        <img src={circle1} alt="circle" />
+      </div>
+      <div className="hidden md:block absolute right-8 top-20 -z-10 ">
+        <img src={circle2} alt="circle" className='w-44' />
       </div>
     </div>
   );
