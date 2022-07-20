6adad5057914641ee84021b323ac150375d211e0
checkiamsiam <issiam02415@gmail.com>
2022-07-20 23:03:38 +0600

    fixing

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,40 +1,88 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../../asset/logo.png'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../asset/Jobs-ladder-logo.png";
+import { Spin as Hamburger } from 'hamburger-react'
+import 'react-modern-drawer/dist/index.css'
+import HeaderDrawer from "./HeaderDrawer";
+
 const Header = () => {
-    return (
-        <div className=''>
-            <div className='container mx-auto'>
-            <div class="navbar bg-base-100">
-                <div class="dropdown navbar-start lg:hidden">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link className='btn border-primary hover:base-100 text-base-100' to="/login">Login</Link></li>
-                    </ul>
-                </div>
-                <div class="lg:navbar-start w-full flex justify-end">
-                    <Link to="/" class=" normal-case text-xl"><img src={logo} alt=""></img></Link>
-                </div>
-                
-                <div class="navbar-end hidden lg:flex">
-                    <ul class="menu menu-horizontal p-0">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                    <li><Link className='btn border-primary hover:base-100' to="/login">Login</Link></li>
-                    </ul>
-                </div>
-            </div>
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false)
+  const activeLink = ({ isActive }) => {
+    return {
+      fontWeight: 500,
+      color: isActive && "#ED3AB2",
+    };
+  };
+  const toggleDrawer = () => {
+    setIsOpenDrawer((prevState) => !prevState)
+  }
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="navbar w-full border-b border-gray-300">
+          <div className="navbar-start">
+            <Link to="/">
+              <img
+                className="w-40 pr-10 lg:border-r border-gray-300"
+                src={logo}
+                alt="Job's Ladder Logo"
+              />
+            </Link>
+            <ul class="hidden lg:flex items-center ml-5">
+              <li>
+                <NavLink
+                  to="/"
+                  className="mx-4 hover:text-primary duration-200"
+                  style={activeLink}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className="mx-4 hover:text-primary duration-200"
+                  style={activeLink}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/blog"
+                  className="mx-4 hover:text-primary duration-200"
+                  style={activeLink}
+                >
+                  Blog
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className="mx-4 hover:text-primary duration-200"
+                  style={activeLink}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <Link
+              className="hidden  md:flex border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300"
+              to="/login"
+            >
+              Login
+            </Link>
+            <button onClick={toggleDrawer} className='ml-2 lg:hidden'>
+              <Hamburger toggled={isOpenDrawer} />
+            </button>
+          </div>
+          <HeaderDrawer isOpen={isOpenDrawer} toggleDrawer={toggleDrawer} />
         </div>
-        </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Header;
