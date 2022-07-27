6ff156b9190487c99fb84abcb478bd4c805dcfec
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-07-27 20:34:00 +0600

    blog

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,11 +1,219 @@
-import React from 'react';
+import React from "react";
+import img from "../../img/guy-shows-document-girl-group-young-freelancers-office-have-conversation-working_146671-13569.webp";
+import img2 from "../../img/img2.webp";
+import img3 from "../../img/img3.webp";
+import img4 from "../../img/img4.webp";
+import img5 from "../../img/img5.jpg";
+import img6 from "../../img/img6.webp";
+import img7 from "../../img/img7.webp";
+import img8 from "../../img/img8.webp";
+import img9 from "../../img/img9.webp";
+import img10 from "../../img/img10.webp";
 
 const Blog = () => {
-    return (
+  return (
+    <div className=" ">
+      <div className="container mx-auto grid lg:grid-cols-2 grid-cols-1 py-14">
         <div>
-            blog
+          <img src={img} alt="" />
         </div>
-    );
+        <div>
+          <div class="card w-96 bg-base-100 mx-auto mt-14">
+            <div class="card-body">
+              <h2 class=" text-5xl">
+                What does it take to build an Employer Brand in 2022?
+              </h2>
+              <p className="text-1xl">
+                With the corporate sector changing rapidly, a strategy for
+                improving the employer brand is critical. The main goal is to
+                gain the competitive advantage your brand requires in the
+                marketplace. Furthermore, as work-from-home and hybrid working
+                are becoming more common, brands must align their objectives
+                accordingly and provide an employee-centric work environment.
+                Employees, particularly millennials,…
+              </p>
+              <div class="card-actions justify-end">
+                <a className="text-[#fc0303]" href="">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto grid lg:grid-cols-3 grid-cols-1 py-14">
+        <div class="card w-96 bg-base-100 shadow-xl ">
+          <figure>
+            <img src={img2} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              How HR Software Increases Productivity & Profit
+            </h2>
+            <p>
+              Effective human resource (HR) management plays a major role in
+              organizational performance. It allows HR managers to find the
+              right talent and handle human resources as effectively as
+              possible. Moreover, it can be instrumental in ensuring smooth
+              business processes and change management.{" "}
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img3} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Workplace culture—How to set up the right culture from Day 0
+            </h2>
+            <p>
+              When it comes to startups, building the right company culture
+              takes on a whole new dimension that can set the tone for things to
+              come. In the focus on the top line and bottom-line growth, this is
+              an aspect that often gets ignored. The advantage in startups or
+              other early-stage enterprises is that the slate is clean, so to
+              speak...
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img4} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Can Sabbaticals cure the rising employee burnout?
+            </h2>
+            <p>
+              The Great Resignation of 2021 indicates that employees have
+              reevaluated their priorities. Work-life balance is no more a mere
+              preference but a must-have. In the grand scheme of things, there
+              has also been an increase in sabbaticals, largely during COVID-19.
+              Let’s look at how these two are connected and if Sabbaticals can
+              solve the increasing employee burnout...
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl my-10 ">
+          <figure>
+            <img src={img5} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Six pros and cons of unlimited PTO for employers
+            </h2>
+            <p>
+              Unlimited PTO is slowly becoming the norm, with many companies
+              offering it, mainly to attract and retain top talent. Before you
+              jump on the bandwagon, consider these six pros and cons that
+              companies have experienced while implementing unlimited PTO and
+              see if unlimited PTO is the right choice for your organization...
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img src={img6} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              The ultimate 4-part toolkit for scaling startup hiring
+            </h2>
+            <p>
+              Standard procedure doesn’t apply when it comes to early-stage
+              startup hiring. There’s no HR team—there’s probably one HR person
+              who coordinates with the hiring managers on a case-to-case basis.
+              ..
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl my-10">
+          <figure>
+            <img src={img7} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Thank you email to candidate after interview | Sample email
+              template
+            </h2>
+            <p>
+              Sending an acknowledgment email after every interview is a great
+              way to maintain communication continuity and offer a great
+              candidate experience. Here is more on why this is important and
+              what exactly to send. ..
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img8} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Tips for managing PTO accruals efficiently
+            </h2>
+            <p>
+              To understand accrual PTO policy better, let’s break down the word
+              ‘Accrual PTO’, where Accrual, in this context, is the accumulation
+              of PTO over time. In an accrual-based PTO policy, employees
+              receive a set amount of time off after completing a certain time
+              in their tenure as an employee. It can be hourly, daily, weekly,
+              biweekly, monthly, or yearly, depending upon its PTO policy...{" "}
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img9} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              How to recruit top talent on a small budget
+            </h2>
+            <p>
+              Setting up and running a business is no easy game. Once you
+              achieve product-market fit and start gaining some traction for
+              your business, you are likely to think about scaling your business
+              and hiring more people. Whether or not you have external funding,
+              recruiting on a budget is one of the most common and pressing...
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src={img10} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              9 Considerations While Hiring Candidate Background Checks Services
+            </h2>
+            <p>
+              What is one thing to consider while hiring services to do
+              candidate background checks? To help you hire the right candidate
+              background checks service, we asked HR leaders and business owners
+              this question for their best insights. Based on the length of
+              service to the industries served...
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class=" container mx-auto flex flex-col w-full border-opacity-50">
+          <div class="divider text-[#fc0303]">Load More</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
