8841d3707e4024afa3a80f7f981718cc2e2e6b22
shakil18046 <73681343+shakil18046@users.noreply.github.com>
2022-07-28 10:57:59 +0600

    circular

--- src/pages/Dashboard-pages/Circulars/Circulars.js
+++ src/pages/Dashboard-pages/Circulars/Circulars.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import "./Circulars.css"
 const Circulars = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
@@ -8,16 +8,21 @@ const Circulars = () => {
     console.log(watch("example"));
     return (
         <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form className='text-center' onSubmit={handleSubmit(onSubmit)}>
                 {/* register your input into the hook by invoking the "register" function */}
-                <input type="text" placeholder="job title" defaultValue="test" className='border' {...register("example")} />
-                
+                <input type="text" placeholder="job title" defaultValue="test" className='border mb-5 input input-bordered input-secondary w-full max-w-xs' {...register("example")} />
                 {/* include validation with required or other standard HTML validation rules */}
-                <input {...register("exampleRequired", { required: true })} />
+                <br></br>
+                <input type="text" placeholder='job type' className='input  input-bordered mb-5 input-secondary w-full max-w-xs' {...register("exampleRequired", { required: true })} />
+                <br></br>
+                <input type="text" placeholder='institution' className='input  input-bordered mb-5 input-secondary w-full max-w-xs' {...register("exampleRequired", { required: true })} />
+                <br></br>
+                <textarea type="text" placeholder='job description' className='input h-40 input-bordered mb-5 input-secondary w-full max-w-xs' {...register("exampleRequired", { required: true })} />
                 {/* errors will return when field validation fails  */}
+                <br></br>
                 {errors.exampleRequired && <span>This field is required</span>}
-                
-                <input type="submit" />
+                <br></br>
+                <input className='btn bg-primary text-base-100 hover:bg-primary' type="submit" />
             </form>
         </div>
     );
--- src/pages/Dashboard-pages/JobPost/JobPost.js
+++ src/pages/Dashboard-pages/JobPost/JobPost.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import {useSelector} from "react-redux";
+import store from '../../../store';
 const JobPost = () => {
+    const jobpost = useSelector((state)=> store.jobpostReducer)
+    console.log(jobpost);
     return (
         <div>
             job post
