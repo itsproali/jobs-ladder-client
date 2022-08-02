21c8d9faaab8704c84b8f3ef376eae825857dad3
Mohammad Ali <itsproali@gmail.com>
2022-08-02 08:51:33 +0600

    ♻️

--- src/pages/Welcome/CollectInfo.js
+++ src/pages/Welcome/CollectInfo.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import welcome from "../../asset/welcome.png";
