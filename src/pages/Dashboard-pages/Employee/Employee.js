157c5f18d2ad1cd190f690f16cade16972530ade
checkiamsiam <issiam02415@gmail.com>
2022-08-05 10:57:23 +0600

    tuktak changes

--- src/pages/Dashboard-pages/Employee/Employee.js
+++ src/pages/Dashboard-pages/Employee/Employee.js
@@ -1,10 +1,11 @@
 import React from "react";
 const Employee = () => {
   return (
-    <div className="grid grid-cols-3 ">
+    <div className="grid grid-cols-3 gap-5">
 
-      <div className=' border-solid border-2 border-indigo-600'>
-        <div className='ml-11 mt-6 mb-5'>
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
           <div class="avatar">
             <div class="w-34 rounded-full ">
               <img src="https://placeimg.com/192/192/people" />
@@ -21,14 +22,155 @@ const Employee = () => {
                 <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
                 <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                 <div class="modal-action">
-                  <button href="#" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</a>
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
                 </div>
               </div>
             </div>
           </div>
+          </div>
         </div>
       </div>
-    </div >
+
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
+          <div class="avatar">
+            <div class="w-34 rounded-full ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className="ml-7 mt-4 text-2xl">company name</h2>
+          <p className="ml-12 mt-1 text-xl">Designation</p>
+
+          <div>
+            <a href="#my-modal-2" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl">aLLow</a>
+
+            <div class="modal" id="my-modal-2">
+              <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div class="modal-action">
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
+          <div class="avatar">
+            <div class="w-34 rounded-full ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className="ml-7 mt-4 text-2xl">company name</h2>
+          <p className="ml-12 mt-1 text-xl">Designation</p>
+
+          <div>
+            <a href="#my-modal-2" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl">aLLow</a>
+
+            <div class="modal" id="my-modal-2">
+              <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div class="modal-action">
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
+          <div class="avatar">
+            <div class="w-34 rounded-full ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className="ml-7 mt-4 text-2xl">company name</h2>
+          <p className="ml-12 mt-1 text-xl">Designation</p>
+
+          <div>
+            <a href="#my-modal-2" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl">aLLow</a>
+
+            <div class="modal" id="my-modal-2">
+              <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div class="modal-action">
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
+          <div class="avatar">
+            <div class="w-34 rounded-full ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className="ml-7 mt-4 text-2xl">company name</h2>
+          <p className="ml-12 mt-1 text-xl">Designation</p>
+
+          <div>
+            <a href="#my-modal-2" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl">aLLow</a>
+
+            <div class="modal" id="my-modal-2">
+              <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div class="modal-action">
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+
+      <div className=' border-solid rounded-md border-2 border-indigo-600'>
+        <div className=' flex justify-center items-center mt-6 mb-5 '>
+          <div>
+          <div class="avatar">
+            <div class="w-34 rounded-full ">
+              <img src="https://placeimg.com/192/192/people" />
+            </div>
+          </div>
+          <h2 className="ml-7 mt-4 text-2xl">company name</h2>
+          <p className="ml-12 mt-1 text-xl">Designation</p>
+
+          <div>
+            <a href="#my-modal-2" className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl">aLLow</a>
+
+            <div class="modal" id="my-modal-2">
+              <div class="modal-box">
+                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                <div class="modal-action">
+                  <button className="btn border-2 border-primary text-primary rounded-md py-2 px-12 hover:bg-primary hover:border-primary hover:text-white duration-300 uppercase ml-6 mt-8 text-xl" >Yay!</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
