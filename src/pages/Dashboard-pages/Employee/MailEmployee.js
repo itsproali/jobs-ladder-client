29d098b5d7a05b7815e398f66a0553b0931836ea
checkiamsiam <issiam02415@gmail.com>
2022-08-26 11:32:27 +0600

    send mail employee form edit

--- src/pages/Dashboard-pages/Employee/MailEmployee.js
+++ src/pages/Dashboard-pages/Employee/MailEmployee.js
@@ -2,54 +2,50 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { useSelector } from "react-redux";
 
-
 const MailEmployee = () => {
   const currentEmployee = useSelector((state) => state.currentEmployeeData);
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_dm833yz",
-        "template_9ffqtnf",
-        form.current,
-        "3CMrtQpNGTWZ1GDPc"
-      )
-      .then(
-        (result) => {
-          console.log(result);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    emailjs.sendForm("service_dm833yz", "template_9ffqtnf", form.current, "3CMrtQpNGTWZ1GDPc").then(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-        <label className="block text-gray-700 text-sm mb-2 ">Name</label>
+    <div className="bg-gradient-to-tl from-secondary to-primary  ">
+      <div className="px-5 min-h-screen flex justify-center items-center max-w-lg mx-auto sm:py-10  py-20">
+        <form ref={form} onSubmit={sendEmail} className="w-full">
+          <label className="block text-white text-sm mb-2 ">Name</label>
 
-        <input
-          className="block w-full bg-white text-gray-700 border border-gray-400 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500"
-          type="text"
-          name="user_name"
-        />
-        <label className="block text-gray-700 text-sm mb-2 ">Email</label>
-        <input
-          className="block w-full bg-white text-gray-700 border border-gray-400 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500"
-          type="email"
-          name="user_email"
-          value={currentEmployee?.email}
-        />
-        <label className="block text-gray-700 text-sm mb-2 ">Message</label>
-        <textarea
-          style={{ height: "180px" }}
-          className="block w-full h-14 bg-white text-gray-700 border border-gray-400 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500"
-          name="message"
-        />
-        <input className="btn btn-primary" type="submit" value="Send" />
-      </form>
+          <input
+            placeholder="Enter Name"
+            className="block w-full  bg-white text-gray-700 border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-primary"
+            type="text"
+            name="user_name"
+          />
+          <label className="block text-white text-sm mb-2 ">Email</label>
+          <input
+            className="block w-full bg-white text-gray-700 border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:border-primary"
+            type="email"
+            name="user_email"
+            value={currentEmployee?.email}
+          />
+          <label className="block text-white text-sm mb-2 ">Message</label>
+          <textarea
+            placeholder="type message..."
+            style={{ height: "180px" }}
+            className="block w-full h-14 bg-white text-gray-700 border border-gray-400 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-primary"
+            name="message"
+          />
+          <input className="btn btn-primary w-full" type="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
 };
