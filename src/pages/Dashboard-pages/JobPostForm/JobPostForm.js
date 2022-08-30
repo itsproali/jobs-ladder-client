e9ad4c25244d4a8ab509415d28936f4f8911f2e6
checkiamsiam <issiam02415@gmail.com>
2022-08-30 09:50:38 +0600

    bug fixing

--- src/pages/Dashboard-pages/JobPostForm/JobPostForm.js
+++ src/pages/Dashboard-pages/JobPostForm/JobPostForm.js
@@ -41,8 +41,8 @@ const JobPostForm = () => {
       date: date.getTime(),
     };
     await fetching.post("/job-post", jobData);
-    await dispatch(getJobPosts());
     await navigate("/dashboard/job-post", { replace: true });
+    await dispatch(getJobPosts());
   };
 
   return (
